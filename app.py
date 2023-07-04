9da51d21839fedddc40847a7aef5f8e073b5ba87
AbhisheKumar1616 <63004757+AbhisheKumar1616@users.noreply.github.com>
2023-07-04 11:19:36 +0000

    added jsonify function to return json formatted data to server and added dynamicness to the app by adding for loop to display jobs and also added a job_div for better readability of code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app=Flask(__name__)
 
@@ -12,7 +12,8 @@ jobs=[
   {
     "ID":2,
     "Title":"Flask Developer",
-    "Location":"Delhi, India"
+    "Location":"Delhi, India",
+    "Salary": "Rs. 8,00,000"
   },
   {
     "ID":3,
@@ -21,10 +22,15 @@ jobs=[
     "Salary":"Rs. 16,00,000"
   }
 ]
-
+#returns a html formated data to the server
 @app.route("/")
 def helloworld():
   return render_template("home.html", jobs=jobs,company="JobShala")
+
+# return a json formated data to the server
+@app.route("/api/jobs")
+def jobsdefi():
+  return jsonify(jobs)
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug=True)
