d30d1c94dcf8c3000eb6a2e18ef6fd014b36ccda
AbhisheKumar1616 <63004757+AbhisheKumar1616@users.noreply.github.com>
2023-07-04 08:24:13 +0000

    add job vacancies using list as for now

--- app.py
+++ app.py
@@ -2,9 +2,29 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+jobs=[
+  {
+    "ID":1,
+    "Title":"Python Developer",
+    "Location":"Bengaluru, India",
+    "Salary":"Rs. 6,00,000"
+  },
+  {
+    "ID":2,
+    "Title":"Flask Developer",
+    "Location":"Delhi, India"
+  },
+  {
+    "ID":3,
+    "Title":"Backend Developer",
+    "Location":"Bengaluru, India",
+    "Salary":"Rs. 16,00,000"
+  }
+]
+
 @app.route("/")
 def helloworld():
-  return render_template("home.html")
+  return render_template("home.html", jobs=jobs,company="JobShala")
 
 if __name__=="__main__":
   app.run(host="0.0.0.0", debug=True)
